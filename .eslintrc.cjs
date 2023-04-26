9f08fa3a1adb02c66101c3536a374c9500e2e7a0
juancgalueweb <juancgalue@icloud.com>
2023-04-26 12:19:45 -0400

    Empezando con la app

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,8 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
   },
   settings: {
     react: {
